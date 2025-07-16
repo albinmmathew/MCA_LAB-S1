94bf4847e5051e02738bc7f0937320cefe37a085
unknown <albinmmathew@gmail.com>
2025-07-16 15:24:10 +0530

    Change in Q2

--- DATA STRUCTURES/PROGRAM 3.c
+++ DATA STRUCTURES/PROGRAM 3.c
@@ -1,52 +1,76 @@
 #include<stdio.h>	//declaring variable positionof top element 
-void insert(int e,int s[5],int top){		//Function to insert element into stack
+int insert(int e,int stack[5],int top){		//Function to insert element into stack
 	if(top+1==5){
 		printf("Error : Stack is Full");
 	}
 	else{
-		s[++top]=e; 
+		stack[++top]=e; 
 	}
+	return top;
 }
-void erase(int top,int s[5]) //function to delete top element
+int erase(int top,int stack[5]) //function to delete top element
 {
 	if (top==-1){
 		printf("Error: Stack is empty");
 	}
 	else{
-		printf("\n %d",s[top--]);
+		printf("\n %d",stack[top--]);
 	}
+	return top;
 }
-void display(int top,int s[5]){ //function to display the elements in stack
+void display(int top,int stack[5]){ //function to display the elements in stack
 	if (top==-1)
 		printf("Empty Stack");
 	else{
-		for(int i=0;i<=top;i++){
-			printf("%d \t",s[i]);
+		int i;
+		for(i=0;i<=top;i++){
+			printf("%d \t",stack[i]);
 		}
 		printf("\n");
 	}
 }
+void search(int stack[5],int top,int b){
+	int isfound=0;
+	if (top==-1)
+		printf("Empty Stack");
+	int i;
+	for(i=0;i<=top;i++){
+		if (b==stack[i]){
+			isfound=1;
+			printf("Element found at [%d] position.\n",i);
+		}
+	}
+	if (isfound=0){
+		printf("Element not found.");
+	}
+}
 int menu(){ //function for menu
 	int ch;
-	printf("\n INSERT-1 \n DELETE-2 \n DISPLAY-3 \n EXIT -4 \n Enter your choice : ");
+	printf("\n INSERT-1 \n DELETE-2 \n DISPLAY-3 \n SEARCH-4 \n EXIT -5 \n Enter your choice : ");
 	scanf("%d",&ch);
 	return ch;
 }
-void processStack(int s[5]){ //working of menu
+void processStack(int stack[5]){ //working of menu
 	int ch;
+	int b;
 	int top=-1;
-	for (ch=menu();ch!=4;ch=menu()){
+	for (ch=menu();ch!=5;ch=menu()){
 		switch(ch){
 			case 1: //insert 
 				printf("Enter the value to insert : ");
 				scanf("%d",&ch);
-				insert(ch,s,top);
+				insert(ch,stack,top);
 				break;
 			case 2: //delete
-				erase(top,s);
+				erase(top,stack);
 				break;
 			case 3: //display
-				display(top,s);
+				display(top,stack);
+				break;
+			case 4: //search
+				printf("Enter the value to search : ");
+				scanf("%d",&b);
+				search(stack,top,b);
 				break;
 			default://any other options
 				printf("Error: Wrong Choice");
