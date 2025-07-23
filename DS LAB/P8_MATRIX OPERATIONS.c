aa03ffbc57edc37205c88572e20f2b745c698e40
albinmmathew <albinmmathew@gmail.com>
2025-07-23 15:52:57 +0530

    ^_^

--- DS LAB/P8_MATRIX OPERATIONS.c
+++ DS LAB/P8_MATRIX OPERATIONS.c
@@ -1,6 +1,16 @@
-/*PROGRAM-8 MATRIX OPERATIONS
+/*PROGRAM-8 MATRIX ADDITION AND SUBTRACTION
 @ALBIN MAMMEN MATHEW
 23/07/2025
 */
+
 #include<stdio.h>
+int a[3][3],b[3][3],sum[3][3];
+void add(){
+	int i,j;
+	for (i=0;i<3;1++){
+		for(j=0;)
+	}
+}
+
+
 
