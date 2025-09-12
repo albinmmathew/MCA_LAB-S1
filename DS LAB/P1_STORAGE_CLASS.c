842d4234d9f90ca9840e67a47fe0d6cd5bea2f09
albinmmathew <albinmmathew@gmail.com>
2025-09-12 23:46:25 +0530

    DS P21

--- DS LAB/P1_STORAGE_CLASS.c
+++ DS LAB/P1_STORAGE_CLASS.c
@@ -1,3 +1,4 @@
+
 /*PROGRAM 1 USE DIFFERENT STORAGE CLASSES (LOCAL,GLOBAL,STATIC,REGISTER)
 @ALBIN MAMMEN MATHEW
 16/07/2025
@@ -10,8 +11,8 @@ void disp(){
 	printf("this is a local variable : %d \n",i);
 }
 int main(){
-	static int c;//intialising static varible
-	register int p=4; //intialising register variable
+	static int c;	//intialising static varible
+	register int p=4; 	//intialising register variable
 	printf("this is a static variable : %d \n",c);
 	printf("this is a global variable : %d \n",a);
 	printf("this is a register variable : %d \n",p);
